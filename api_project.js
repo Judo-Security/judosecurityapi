6ac3447387cd7d6bdcb8f81d5c6cb4d8e1b9eb32
Hemangi <hemangi.khakhi@ajmerainfotech.com>
2020-09-04 14:57:49 +0530

    Update API doc changes.

--- api_project.js
+++ api_project.js
@@ -16,7 +16,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-09-04T07:13:00.903Z",
+    "time": "2020-09-04T09:27:14.215Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
